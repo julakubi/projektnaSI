04413d3e62fa1f06100a106a3a4b303dacde9e5e
julakubi <julakubi@gmail.com>
2020-06-10 17:36:03 +0200

    User login

---
+++ app/src/Entity/User.php
@@ -0,0 +1,180 @@
+<?php
+/**
+ * User entity.
+ */
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @ORM\Table(
+ *     name="users",
+ *     uniqueConstraints={
+ *     @ORM\UniqueConstraint(
+ *     name="username_idx",
+ *     columns={"username"},
+ *          )
+ *    }
+ * )
+ * @UniqueEntity(fields={"username"})
+ */
+class User implements UserInterface
+{
+
+    /**
+     * Role user.
+     *
+     * @var string
+     */
+    const ROLE_USER = 'ROLE_USER';
+    /**
+     * Role admin.
+     *
+     * @var string
+     */
+    const ROLE_ADMIN = 'ROLE_ADMIN';
+
+    /**
+     * Primary key.
+     *
+     * @var int
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(
+     *     name="id",
+     *     type="integer",
+     *     nullable=false,
+     *     options={"unsigned"=true},
+     * )
+     */
+    private $id;
+
+    /**
+     * Username.
+     *
+     * @var string
+     *
+     * @ORM\Column(type="string",
+     *      length=180,
+     *      unique=true
+     *     )
+     */
+    private $username;
+
+    /**
+     * Roles.
+     *
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
+
+    /**
+     * The hashed password.
+     *
+     * @var string
+     *
+     * @ORM\Column(type="string")
+     */
+    private $password;
+
+    /**
+     * Getter for Id.
+     *
+     * @return int|null Result
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     *
+     * @return string User name
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->username;
+    }
+
+    /**
+     * Setter fo Username.
+     *
+     * @param string $username Username
+     */
+    public function setUsername(string $username): void
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * Getter for Roles.
+     *
+     * @see UserInterface
+     *
+     * @return array Roles
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    /**
+     * Setter for Roles.
+     *
+     * @param array $roles Roles
+     */
+    public function setRoles(array $roles): void
+    {
+        $this->roles = $roles;
+    }
+
+    /**
+     * Getter for the Password.
+     *
+     * @see UserInterface
+     *
+     * @return string|null Password
+     */
+    public function getPassword(): string
+    {
+        return (string) $this->password;
+    }
+
+    /**
+     * Setter for the Password.
+     *
+     * @param string $password Password
+     */
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getSalt()
+    {
+        // not needed when using the "bcrypt" algorithm in security.yaml
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+}
